f3157b65422ee115ba4a9a4d7a9ac32dc7cc3d17
Maitreya444 <maitreyg.comp_ioe@bkc.met.edu>
2026-04-20 17:12:29 +0530

    Added song and marathi msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,24 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  isPlaying = false;
+  youtubeUrl!: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  toggleMusic() {
+    this.isPlaying = !this.isPlaying;
+
+    if (this.isPlaying) {
+      const url = 'https://www.youtube.com/embed/8zdg-pDF10g?autoplay=1&loop=1&playlist=8zdg-pDF10g';
+      this.youtubeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    }
+  }
+}
